1a6190e754b7acde28d5056678de6fdb837cdd6d
iamcelestino <trcelestino488@gmail.com>
2024-12-14 01:51:23 -0800

    initial commit

---
+++ App/views/login.php
@@ -0,0 +1,12 @@
+<?php $this->view('partials/header') ?>
+    <main>
+        <section class="">
+            <div class="container">
+                <div>
+                    
+                </div>
+            </div>
+
+        </section>
+    </main>
+<?php $this->view('partials/footer') ?>
